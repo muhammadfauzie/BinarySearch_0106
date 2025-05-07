4822268825ce2a52aa90f034163d80e26caf6a82
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-07 12:28:59 +0700

    void binary

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -49,3 +49,66 @@ void bubbleSortArray()
     } while (pass <= nPanjang - 1);
 }
 void display()
+{
+    cout << "\n====================================\n";
+    cout << "   Elemen Array Setelah Diurutkan (Asc)   \n";
+    cout << "====================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " ==> ";
+        }
+    }
+    cout << endl;
+}
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n====================================\n";
+        cout << "         Pencarian Binary Search    \n";
+        cout << "====================================\n";
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Elemen " << x << " tidak ditemukan dalam array.\n";
+        }
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
+}
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    binarySearch();
+}
