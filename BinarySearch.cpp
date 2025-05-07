59f856ef3933a9e59394340df38b72a6099bf30d
fauzie primarka <fauzieprimarka@gmail.com>
2025-05-07 12:21:35 +0700

    cout

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,3 +21,8 @@ void input()
         }
     }
 }
+
+cout << "\n====================================\n";
+cout << "        Masukkan Elemen Array       \n";
+cout << "====================================\n";
+for (int i = 0; i < nPanjang; i++)
